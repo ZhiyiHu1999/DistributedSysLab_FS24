be9aefb919334309a45b114049913cd682587e01
ZhiyiHu1999 <huzhiyi1037@gmail.com>
2024-09-19 06:26:26 +0200

    modify job script to export sqlite files with time synchronization by setting --ts-normalize=true

--- nccl_example/job_example_2_ault.sh
+++ nccl_example/job_example_2_ault.sh
@@ -79,7 +79,7 @@ for report_file in ${NSYS_REPORT_DIR}/*.nsys-rep; do
   if [ -f "$report_file" ]; then
     sqlite_file="${report_file%.nsys-rep}.sqlite"
     # srun ~/opt/nvidia/nsight-systems-cli/2024.5.1/bin/nsys export --sqlite "$sqlite_file" "$report_file"
-    ~/opt/nvidia/nsight-systems-cli/2024.5.1/bin/nsys export --type=sqlite --output="$sqlite_file" "$report_file"
+    ~/opt/nvidia/nsight-systems-cli/2024.5.1/bin/nsys export --type=sqlite --ts-normalize=true --output="$sqlite_file" "$report_file"
     echo "Exported $report_file to $sqlite_file"
   fi
 done

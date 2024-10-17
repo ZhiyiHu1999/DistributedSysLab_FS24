1c83d8382cd1da968e4ec76f4060e16827a81f28
ZhiyiHu1999 <huzhiyi1037@gmail.com>
2024-10-17 12:01:17 +0200

    add allgather and broadcast examples

--- nccl_example/example_2.508743_no_trace_2MB_loop_for_100.e
+++
@@ -1,5 +0,0 @@
-tee: /log.txt: Permission denied
-
-real	3m22.415s
-user	0m0.007s
-sys	0m0.014s
---
+++ nccl_example/example_broadcast/job_example_braodcast_ault_nsys.sh
@@ -0,0 +1,59 @@
+#!/bin/bash -l
+#
+#SBATCH --job-name="nccl_example_broadcast"
+#SBATCH --time=02:10:00
+#SBATCH --partition=amdrtx
+#SBATCH --nodelist=ault[42-44]
+#SBATCH --ntasks-per-node=1
+#SBATCH --gpus-per-task=1
+#SBATCH --output=example_broadcast.%j.o
+#SBATCH --error=example_broadcast.%j.e
+#SBATCH --account=g34
+
+module load openmpi/4.1.1
+# module load cuda/11.6.2
+module load cuda/11.8.0
+# module load cuda/12.1.1
+module load rdma-core/34.0
+module load python/3.8.12
+
+srun nvidia-smi -L
+
+rm -rf "./results"
+mkdir -p "./results"
+
+export NCCL_ALGO=Tree  ## ncclBroadcast only use Ring topology
+export NCCL_PROTO=Simple
+# export NCCL_MIN_NCHANNELS=4
+export NCCL_MAX_NCHANNELS=1
+export NCCL_DEBUG=INFO ## For debug
+export NCCL_TOPO_DUMP_FILE="./results/Topology_Intra_Node.txt" ## NCCL_PARAM(TopoDumpFileRank, "TOPO_DUMP_FILE_RANK", 0);
+export NCCL_GRAPH_DUMP_FILE="./results/Graph.txt" ## NCCL_PARAM(GraphDumpFileRank, "GRAPH_DUMP_FILE_RANK", 0);
+
+export MPI_ROOT=/apps/ault/spack/opt/spack/linux-centos8-zen/gcc-8.4.1/openmpi-4.1.1-epxpvnwjl2smjwuwqg67h2wrmdxw6nhj
+
+export NCCL_ROOT=/users/zhu/nccl_nvtx/nccl/build
+export LD_LIBRARY_PATH=/users/zhu/nccl_nvtx/nccl/build/lib:$LD_LIBRARY_PATH
+
+nvcc -I${MPI_ROOT}/include -L${MPI_ROOT}/lib -lmpi -I${NCCL_ROOT}/include -L${NCCL_ROOT}/lib -lnccl example_broadcast.cu -o example_broadcast
+
+# export NSYS_REPORT_DIR="/users/zhu/DistributedSysLab_FS24/nccl_example/results/nsys_reports"
+export NSYS_REPORT_DIR="./results/nsys_reports"
+rm -rf $NSYS_REPORT_DIR
+mkdir -p $NSYS_REPORT_DIR
+
+# # time srun ~/opt/nvidia/nsight-systems-cli/2024.5.1/bin/nsys profile --trace=nvtx,cuda -s none --output=${NSYS_REPORT_DIR}/example_broadcast_nsys_report_%h_%p ./example_broadcast
+srun ~/opt/nvidia/nsight-systems-cli/2024.5.1/bin/nsys profile --trace=nvtx,cuda -s none --output=${NSYS_REPORT_DIR}/example_broadcast_nsys_report_%h_%p ./example_broadcast
+
+for report_file in ${NSYS_REPORT_DIR}/*.nsys-rep; do
+  if [ -f "$report_file" ]; then
+    sqlite_file="${report_file%.nsys-rep}.sqlite"
+    # srun ~/opt/nvidia/nsight-systems-cli/2024.5.1/bin/nsys export --sqlite "$sqlite_file" "$report_file"
+    ~/opt/nvidia/nsight-systems-cli/2024.5.1/bin/nsys export --type=sqlite --ts-normalize=true --output="$sqlite_file" "$report_file"
+    echo "Exported $report_file to $sqlite_file"
+  fi
+done
+
+# python3 parser_sqlite2goal.py
+
+# python3 goal2dot.py

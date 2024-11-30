5409e679f1d1c998c587ec01d7909b676cd280fa
ZhiyiHu1999 <huzhiyi1037@gmail.com>
2024-11-30 23:12:02 +0100

    modify job script to enable pnccl tracing

--- nccl_example/example_allreduce/job_example_allreduce_ault_tracing.sh
+++ nccl_example/example_allreduce/job_example_allreduce_ault_tracing.sh
@@ -3,8 +3,8 @@
 #SBATCH --job-name="nccl_example_allreduce"
 #SBATCH --time=02:10:00
 #SBATCH --partition=amdrtx
-#SBATCH --nodelist=ault[42-44]
-#SBATCH --ntasks-per-node=1
+#SBATCH --nodelist=ault[43-44]
+#SBATCH --ntasks-per-node=2
 #SBATCH --gpus-per-task=1
 #SBATCH --output=example_allreduce.%j.o
 #SBATCH --error=example_allreduce.%j.e
@@ -22,10 +22,10 @@ srun nvidia-smi -L
 rm -rf "./results"
 mkdir -p "./results"
 
-export NCCL_ALGO=Tree
-export NCCL_PROTO=LL
-# export NCCL_MIN_NCHANNELS=4
-export NCCL_MAX_NCHANNELS=1
+export NCCL_ALGO=Ring
+export NCCL_PROTO=Simple
+export NCCL_MIN_NCHANNELS=4
+# export NCCL_MAX_NCHANNELS=1
 export NCCL_DEBUG=INFO ## For debug
 export NCCL_TOPO_DUMP_FILE="./results/Topology_Intra_Node.txt" ## NCCL_PARAM(TopoDumpFileRank, "TOPO_DUMP_FILE_RANK", 0);
 export NCCL_GRAPH_DUMP_FILE="./results/Graph.txt" ## NCCL_PARAM(GraphDumpFileRank, "GRAPH_DUMP_FILE_RANK", 0);
@@ -33,7 +33,11 @@ export NCCL_GRAPH_DUMP_FILE="./results/Graph.txt" ## NCCL_PARAM(GraphDumpFileRan
 export MPI_ROOT=/apps/ault/spack/opt/spack/linux-centos8-zen/gcc-8.4.1/openmpi-4.1.1-epxpvnwjl2smjwuwqg67h2wrmdxw6nhj
 
 export NCCL_ROOT=/users/zhu/nccl_nvtx_npkit/nccl/build
-export LD_LIBRARY_PATH=/users/zhu/nccl_nvtx_npkit/nccl/build/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/apps/ault/spack/opt/spack/linux-centos8-zen/gcc-8.4.1/cuda-11.8.0-fjdnxm6yggxxp75sb62xrxxmeg4s24ml/lib64:/users/zhu/nccl_nvtx_npkit/nccl/build/lib:$LD_LIBRARY_PATH
+# export NCCL_ROOT=/users/zhu/nccl/build
+# export LD_LIBRARY_PATH=/users/zhu/nccl/build/lib:$LD_LIBRARY_PATH
+export LD_PRELOAD=/users/zhu/nccl-tracer/src/libncclprof.so
+export NCCL_TRACE_PREFIX=/users/zhu/DistributedSysLab_FS24/nccl_example/example_allreduce/results/result
 
 nvcc -I${MPI_ROOT}/include -L${MPI_ROOT}/lib -lmpi -I${NCCL_ROOT}/include -L${NCCL_ROOT}/lib -lnccl example_allreduce.cu -o example_allreduce
 
@@ -68,9 +72,9 @@ done
 
 # python3 get_traced_events.py
 
-python3 goal_generator.py
+# python3 goal_generator.py
 
-python3 goal2dot.py
+# python3 goal2dot.py
 
 # python3 ../trace_generator_npkit.py --npkit_dump_dir=$npkit_dump_dir\
 #                                  --npkit_event_header_path="/users/zhu/nccl_nvtx_npkit/nccl/src/include/npkit/npkit_event.h"\

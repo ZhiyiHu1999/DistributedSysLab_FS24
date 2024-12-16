55e438621682d003921d1f7d4e5bca1dd313887b
ZhiyiHu1999 <huzhiyi1037@gmail.com>
2024-12-16 09:58:42 +0100

    add dot visualize job script

---
+++ deepspeed_example/example_hellodeepspeed/job_dot_visualize.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+#
+#SBATCH --job-name="dot_visualize"
+#SBATCH --time=24:00:00
+#SBATCH --partition=amdrtx
+#SBATCH --nodelist=ault[44]
+#SBATCH --ntasks-per-node=1
+#SBATCH --gpus-per-task=1
+#SBATCH --mem=200G
+#SBATCH --output=dot_visualize.%j.o
+#SBATCH --error=dot_visualize.%j.e
+#SBATCH --account=g34
+
+srun dot -Tsvg ./results/Events_Dependency.dot -o ./results/Events_Dependency.svg
